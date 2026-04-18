ec9d91bb18b2de3f7921018f91a9becb9e6276fc
ankit3131 <ankitchauhan21293@gmail.com>
2026-04-18 09:46:07 +0530

    all string

--- for-loop/remove.js
+++
@@ -1,8 +0,0 @@
-let str = "ankit";
-let newstr = str.split("");
-for(let i=0;i<newstr.length;i++) {
-    if(newstr[i]=="a") {
-        newstr.splice();
-    }   
-}
-console.log(newstr.join(""));
---
+++ for-loop/stringRemove.js
@@ -0,0 +1,30 @@
+// solve:-1
+// let string = "ankit";
+
+// let slice = string.slice(1);
+
+// console.log(slice);
+
+// output:-nkit Slice original array not change
+
+//solve:-2
+
+// let string = "ankit";
+
+// let arr = string.split("")
+// console.log(arr);
+
+// let splice = arr.splice(1).join("");
+// console.log(splice);
+
+// output:- nkit but this splice will original change
+
+// solve 3
+
+// let string = "ankit"
+// let arr = [];
+// for(let i=0;i<string.length;i++) {
+//     debugger;
+//     arr.push(string[i]);
+// }
+// console.log(arr);
