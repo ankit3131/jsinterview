59df2fbe7940b1a259e2cd8c82cdeb1ad0701c35
ankit3131 <ankitchauhan21293@gmail.com>
2026-04-20 10:58:14 +0530

    stringremove.js file

--- for-loop/stringRemove.js
+++ for-loop/stringRemove.js
@@ -23,8 +23,35 @@
 
 // let string = "ankit"
 // let arr = [];
-// for(let i=0;i<string.length;i++) {
+// let arr1 = [];
+// for (let i = 0; i < string.length; i++) {
 //     debugger;
+//         // string[i]
 //     arr.push(string[i]);
+//     if (string[i] !== arr[0]) {
+//         debugger;
+//         arr1.push(arr[i])
+//     }
+ 
 // }
-// console.log(arr);
+
+// console.log(arr1)
+
+// output:-[n,k,i,t] on array 
+
+// solve4
+let string = "ankit"
+let removStr = '';
+
+for(let i=0;i<string.length;i++) {
+    debugger;
+    if(string[i] !== string[0]) {
+        removStr += string[i]
+        // console.log(removStr);
+        // removStr++
+    
+    }
+}
+console.log(removStr)
+
+// output will be 'nkit' Good one chatgpt
