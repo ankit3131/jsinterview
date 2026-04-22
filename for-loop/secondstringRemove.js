d5e162cc979ff6a833a29ece5600963624160055
T8 <T8@CC.COM>
2026-04-22 10:51:04 +0530

    seconfstringRemove.js

--- for-loop/secondstringRemove.js
+++ for-loop/secondstringRemove.js
@@ -12,3 +12,23 @@
 // console.log(laststr);
 
 // output:-anki but not good approach
+
+//solve2
+// let str = "ankit"
+// let laststr = ""
+// for(let i=0;i<str.length-1;i++) {
+//    laststr = laststr + str[i];
+// }
+//  console.log(laststr)
+
+//outpu:- anki forloop conditon use 
+
+//solve3
+let str = "ankit"
+let laststr = []
+
+let split = str.split("");
+split.pop();
+ console.log(split.join(""));
+
+ //output:- anki split use , join() can do use
