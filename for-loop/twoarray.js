487051a6916e18b96a27c64c9baee76e0ed17dbb
ankit3131 <ankitchauhan21293@gmail.com>
2026-05-04 22:52:03 +0530

    twoArray

---
+++ for-loop/twoarray.js
@@ -0,0 +1,15 @@
+let arr =  [1,2]
+let arr1 = [3,4]
+let sum = 0;
+
+for(let i=0;i<arr.length;i++) {
+    debugger;
+    sum += arr[i];
+for(let j=0;j<arr1.length;j++) {
+    debugger;
+    sum += arr1[j];
+}
+}
+
+
+console.log(sum)
