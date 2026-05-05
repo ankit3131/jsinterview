9b8e8c031368bb999e890a6eb2fb8614e51f9c24
T8 <T8@CC.COM>
2026-05-05 11:09:05 +0530

    instersectionTwoArray5May.js

---
+++ for-loop/IntersectionofTwoArrays5May.js
@@ -0,0 +1,12 @@
+let arr1 = [1,2,3,4]
+let arr2 = [3,4,5,6]
+let arr3 = [];
+
+for(let i=0;i<arr1.length;i++) {
+    for(let j=0;j<arr2.length;j++) {
+        if(arr1[i] === arr2[j]) {
+arr3.push(arr2[j])
+        }
+    }
+}
+console.log(arr3);
--- for-loop/twoarray.js
+++ for-loop/twoarray.js
@@ -1,15 +1,15 @@
 let arr =  [1,2]
 let arr1 = [3,4]
-let sum = 0;
+let arr3 = []
 
 for(let i=0;i<arr.length;i++) {
-    debugger;
-    sum += arr[i];
 for(let j=0;j<arr1.length;j++) {
-    debugger;
-    sum += arr1[j];
+  if(i === j) {
+let sum = arr[i] + arr1[j];
+arr3.push(sum)
+  }
 }
 }
 
 
-console.log(sum)
+console.log(arr3)
