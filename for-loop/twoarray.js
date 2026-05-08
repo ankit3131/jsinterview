202ae456cd4851ad81ec304877ce902c9c4aa34d
T8 <T8@CC.COM>
2026-05-08 11:05:40 +0530

    twoarrays.js

--- for-loop/twoarray.js
+++ for-loop/twoarray.js
@@ -1,18 +1,35 @@
+// let arr =  [1,2]
+// let arr1 = [3,4]
+// let arr3 = []
+
+// for(let i=0;i<arr.length;i++) {
+// for(let j=0;j<arr1.length;j++) {
+//   if(i === j) {
+// let sum = arr[i] + arr1[j];
+// arr3.push(sum)
+//   }
+// }
+// }
+
+
+// console.log(arr3)
+
+// output: [4,6]
+
 let arr =  [1,2]
 let arr1 = [3,4,5,6]
 let arr3 = []
 
 for(let i=0;i<arr.length;i++) {
-for(let j=0;j<arr1.length;j++) {
-  if(i === j) {
-let sum = arr[i] + arr1[j];
-arr3.push(sum)
-  }
-  if(arr3.includes(arr1[j])) {
-    arr3.push(arr1[j])
+
+  for(let j=0;j<arr1.length;j++) {
+if(i === j) {
+  arr3.push(arr[i] + arr1[j])
+}
+else {
+  debugger;
+  arr3.push(arr1[j])
+}
   }
 }
-}
-
-
 console.log(arr3)
