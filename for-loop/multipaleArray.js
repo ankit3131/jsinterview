e694532407e054bc12c4f5574efcdceb3aa203c5
T8 <T8@CC.COM>
2026-05-11 10:20:05 +0530

    multipale.js

---
+++ for-loop/multipaleArray.js
@@ -0,0 +1,10 @@
+let name = ["ankit1","anniet2","anskirt3","anbkist4"];
+let string = name.join(",");
+for(let i=0;i<string.length;i++) 
+{
+    debugger;
+let remove = string.replace("an" , "");
+console.log(remove);
+}
+
+
