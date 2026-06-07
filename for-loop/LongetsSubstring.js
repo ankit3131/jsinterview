2ad5ae8e590118b1eb3d4759d2cd51e3c078cf57
ankit3131 <ankitchauhan21293@gmail.com>
2026-06-07 18:03:45 +0530

    longetsSubstring

--- for-loop/LongetsSubstring.js
+++ for-loop/LongetsSubstring.js
@@ -1,12 +1,48 @@
-let str = "pwwkew";
-for(let i=0;i<str.length;i++) {
-    for(let j=i+1;j<str.length;j++) {
-        if(str[i] !== str[j]) {
-            debugger;
-            console.log(str[i] , str[j])
-            break;
-        }
-    }
-}
+
+// let str = "abcab";
+// let count = 0;
+// for(let i=0;i<str.length;i++) {
+//     let arr = [];
+//     for(let j=i;j<str.length;j++) {
+//         debugger;
+//         if(arr.includes(str[j])) {  
+//             break;
+//         }
+//         if(count<3) {
+//         arr.push(str[j])
+//         }
+//         if(count>3) {
+//             break;
+//         }
+
+//     }
+//     if(count<3) {
+//     console.log(arr);
+//     count++
+//     }
+// }
 
 //output:-3
+
+let str = "abcab";
+let count = 0;
+for(let i=0;i<str.length;i++) {
+    let arr = [];
+    for(let j=i;j<str.length;j++) {
+        debugger;
+        if(arr.includes(str[j])) {  
+            break;
+        }
+        if(count<3) {
+        arr.push(str[j])
+        }
+        if(count>3) {
+            break;
+        }
+
+    }
+    if(count<3) {
+    console.log(arr);
+    count++
+    }
+}
