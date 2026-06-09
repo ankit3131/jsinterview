11f0953fe6982297b46fa2cebf04302b65e9e54d
T8 <T8@CC.COM>
2026-06-09 10:27:40 +0530

    PalindromicSubstring

--- for-loop/LongetsSubstring.js
+++ for-loop/LongetsSubstring.js
@@ -1,12 +1,18 @@
 let str = "pwwkew";
 for(let i=0;i<str.length;i++) {
-    for(let j=i+1;j<str.length;j++) {
-        if(str[i] !== str[j]) {
-            debugger;
-            console.log(str[i] , str[j])
+    let arr = [];
+    let count = 0;
+    for(let j=i;j<str.length;j++) {
+        debugger;
+        if(arr.includes(str[j])) {
             break;
         }
+        
+        if(count<3) {
+ arr.push(str[j])
+        }
     }
+    console.log(arr)
 }
 
 //output:-3
