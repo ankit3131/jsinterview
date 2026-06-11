51c94070ea7773e9c070459f229a5372b1a878d9
T8 <T8@CC.COM>
2026-06-11 13:51:04 +0530

    index.html

--- for-loop/LongetsSubstring.js
+++ for-loop/LongetsSubstring.js
@@ -28,21 +28,6 @@ let str = "abcab";
 let count = 0;
 for(let i=0;i<str.length;i++) {
     let arr = [];
-<<<<<<< HEAD
-    let count = 0;
-    for(let j=i;j<str.length;j++) {
-        debugger;
-        if(arr.includes(str[j])) {
-            break;
-        }
-        
-        if(count<3) {
- arr.push(str[j])
-        }
-    }
-    console.log(arr)
-}
-=======
     for(let j=i;j<str.length;j++) {
         debugger;
         if(arr.includes(str[j])) {  
@@ -54,7 +39,6 @@ for(let i=0;i<str.length;i++) {
         if(count>3) {
             break;
         }
->>>>>>> 2ad5ae8e590118b1eb3d4759d2cd51e3c078cf57
 
     }
     if(count<3) {
