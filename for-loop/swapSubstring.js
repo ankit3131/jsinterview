3c43f31d4cb3078a82d68eb626e71bc28b43ad98
ankit3131 <ankitchauhan21293@gmail.com>
2026-06-18 22:59:43 +0530

    swapSubstring

---
+++ for-loop/swapSubstring.js
@@ -0,0 +1,6 @@
+function string(str) {
+   let sort =  [...new set(str)]
+console.log(sort)
+}
+                                                                                                                                                             
+console.log(string("ababa"));
