97170f1ba188675ca44dd51c3b39a983b5643656
T8 <T8@CC.COM>
2026-06-19 11:15:35 +0530

    swapSubstring

--- for-loop/swapSubstring.js
+++ for-loop/swapSubstring.js
@@ -1,6 +1,21 @@
 function string(str) {
-   let sort =  [...new set(str)]
-console.log(sort)
+ let sorts = str.split('').sort();
+ let count = 1;
+ let maxcount = 1;
+for(let i=0;i<sorts.length;i++) {
+if(sorts[i] === sorts[i+1]) {
+   count++
+   if(count>maxcount) {
+      maxcount = count;
+   }
+}
+else{
+   count = 1;
+}
+}
+
+  return maxcount;
 }
                                                                                                                                                              
-console.log(string("ababa"));
+console.log(string("ababaecefd"));
+
