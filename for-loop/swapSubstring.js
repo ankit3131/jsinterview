523e14aa20bd7b4c997accfb232bf91f26623568
T8 <T8@CC.COM>
2026-06-22 14:46:50 +0530

    practisequestion

--- for-loop/swapSubstring.js
+++ for-loop/swapSubstring.js
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-=======
 function string(str) {
  let sorts = str.split('').sort();
  let count = 1;
@@ -20,5 +18,3 @@ else{
 }
                                                                                                                                                              
 console.log(string("ababaecefd"));
-
->>>>>>> 97170f1ba188675ca44dd51c3b39a983b5643656
---
+++ practisequestion/revesestring.js
@@ -0,0 +1,68 @@
+//////////// reversestring
+
+// let string = "edcba";
+// let arr = string.split("");
+// let arr1 = [];
+// for(let i=arr.length-1;i>=0;i--) {
+// arr1.push(arr[i])
+// }
+// let string1 = arr1.join("");
+// console.log(string1);
+
+///////////// reverse string abcde in loop//////////////////////////////////
+
+/////////// duplicate(1)
+// let arr = [1,1,1,1,2,2,2,2,3,3,3,3,4];
+// let arr1 = [];
+// for(let i=0;i<arr.length;i++) {
+// if(arr[i] !== arr[i+1]) {
+// arr1.push(arr[i])
+// }
+// }
+// console.log(arr1);
+
+//////////duplicate number[ 1, 2, 3,4]//////////////////////////////////
+
+
+/////////// duplicate(2)
+// let arr = [1,2,1,1,2,3,4];
+// let arr1 = [];
+// for(let i=0;i<arr.length;i++) {
+//     for(let j=0;j<arr.length;j++) {
+//         if(!arr1.includes(arr[j])) {
+//             arr1.push(arr[j])
+//         }
+//     }
+// }
+// console.log(arr1);
+
+//////////duplicate number[ 1, 2, 3 ,4]//////////////////////////////////
+
+
+///////////Count duplicate elements
+
+// let arr = [1,1,1,2,2,2,3,3,3,4];
+// let arr1 = [];
+// for(let i=0;i<arr.length;i++) {
+//    for(let j=i+1;j<arr.length;j++){
+//     if(arr[i]===arr[j] && !arr1.includes(arr[j])) {
+//        arr1.push(arr[j])
+//     }
+//    } 
+// }
+// console.log(arr1);
+
+/////////////////// second largest number ///////////////////////////////
+
+// function arr(arr1) {
+//     let max = 0;
+//     for(let i=0;i<arr1.length-1;i++) {
+// if(arr1[i] > max) {
+//     max = arr1[i]
+// }
+//     }
+//     return max;
+// }
+// console.log(arr([1,5,4,3,6]));
+
+/////////////// 5 number //////////////////////////////////////////////
