77529004e7ed7d3d18b54f7e0d451d1bdcac1948
T8 <T8@CC.COM>
2026-06-25 16:08:16 +0530

    Maximuma string js

--- for-loop/MaximuamString.js
+++ for-loop/MaximuamString.js
@@ -1,23 +1,46 @@
-    function string(word) {
-        let count = 1;
-        
-        for(let i=0;i<word.length;i++) {
-            debugger;
-            let spiltes = word[i].split(" ");
-            // console.log(spiltes);
-            debugger;
-        
-    if(spiltes.length>count) {   
-        debugger;
-        count = spiltes.length;
-        //  console.log(count.length);
-    }
-        }
-        return count;
-    }
+    // // tricks:-1
 
-    let arr = string(["alice and bob love leetcode", "i think so too", "this is great thanks very much"]);
-    console.log(arr);;
+    // function string(word) {
+    //     let count = 1;
+        
+    //     for(let i=0;i<word.length;i++) {
+    //         debugger;
+    //         let spiltes = word[i].split(" ");
+    //         // console.log(spiltes);
+    //         debugger;
+        
+    // if(spiltes.length>count) {   
+    //     debugger;
+    //     count = spiltes.length;
+    //     //  console.log(count.length);
+    // }
+    //     }
+    //     return count;
+    // }
+
+    // let arr = string(["alice and bob love leetcode", "i think so too", "this is great thanks very much"]);
+    // console.log(arr);;
 
 
 // otuput:-6 all most clear
+
+//tricks:-2
+function string(word) {
+    let arr1 = [];
+    let temp = ""
+    for(let i=0;i<word.length;i++) {
+        for(let j=0;j<=word[i].length;j++) {
+            if(word[i][j] !== " "){
+temp += word[i][j]
+            }
+            else{
+                arr1.push(temp);
+                temp="";
+            }
+        }
+        // console.log(word[i])
+    }
+    return arr1;
+}
+  let arr = string(["alice and bob love leetcode", "i think so too", "this is great thanks very much"]);
+    console.log(arr);
