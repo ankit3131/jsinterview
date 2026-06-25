daee7b124cbcc13f43348def33d28aa57e82d3fb
T8 <T8@CC.COM>
2026-06-25 14:22:49 +0530

    Maximuam.js

---
+++ for-loop/MaximuamString.js
@@ -0,0 +1,23 @@
+    function string(word) {
+        let count = 1;
+        
+        for(let i=0;i<word.length;i++) {
+            debugger;
+            let spiltes = word[i].split(" ");
+            // console.log(spiltes);
+            debugger;
+        
+    if(spiltes.length>count) {   
+        debugger;
+        count = spiltes.length;
+        //  console.log(count.length);
+    }
+        }
+        return count;
+    }
+
+    let arr = string(["alice and bob love leetcode", "i think so too", "this is great thanks very much"]);
+    console.log(arr);;
+
+
+// otuput:-6 all most clear
