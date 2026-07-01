20f5728f42e9ad89a0c5af25a0ee9c1933e279ff
T8 <T8@CC.COM>
2026-07-01 12:26:17 +0530

    object folder create

---
+++ object/Addproperty.js
@@ -0,0 +1,33 @@
+/////////////// step:1
+// let user = {
+//   name: "Ankit"
+// };
+// user['age'] = 25;
+// console.log(user);
+//////////// add age:25
+
+///////////// step:2
+// let user = {
+//   name: "Ankit"
+// };
+// let age = 25
+
+// user['age'] = age
+
+// console.log(user);
+
+//////////// add age:25
+
+/////////////// step:3
+// let user = {
+//     name:"Ankit"
+// };
+
+// let users = {
+//     ...user , age:25
+// }
+
+// console.log(users)
+
+//////////// add age:25
+
---
+++ object/createobject.js
@@ -0,0 +1,21 @@
+///////// step:-1
+// let user = {}
+// user.name = "Ankit"
+// user.age = 25,
+// user.city = "noida"
+
+// console.log(user);
+
+// {
+//   name: "Ankit",
+//   age: 25,
+//   city: "Noida"
+// }
+
+// /////step:-2
+let user = {}
+user["name"] = "Ankit",
+user["age"] = 25,
+user["city"] = "noida"
+
+console.log(user);
