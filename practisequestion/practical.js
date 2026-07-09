ed18db7bf19c32616a19962362e6d47bc9b78738
T8 <T8@CC.COM>
2026-07-09 15:05:39 +0530

    testpractical

--- practisequestion/practical.js
+++ practisequestion/practical.js
@@ -132,25 +132,54 @@
 
 ///////////missing number 
 
-function missing(arr) {
-    for(let i=0;i<arr.length;i++) {
-        for(let j=0;j<arr.length;j++) {
-            if(arr[i] < arr[j]) {
-                let temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
-            }
+// function missing(arr) {
+//       let missing = [];
+//     for(let i=0;i<arr.length;i++) {
+//         for(let j=0;j<arr.length;j++) {
+//             if(arr[i] < arr[j]) {
+//                 let temp = arr[i];
+//                 arr[i] = arr[j];
+//                 arr[j] = temp;
+//             }
             
-        }
-    }
-    let count = 0;
-    for(let i=0;i<6;i++) {
-if(!missing.includes())
-    }
-    return arr;
-    }
-let num = missing([0,7,4,5,6,4]);
-console.log(num)
+//         }
+//     }
+//     let count = 0;
+  
+//     for(let i=0;i<6;i++) {
+// for(let i=0;i<arr.length;i++) {
+//     if(count[i] !== i) {
+// missing.push(i)
+//     }
+// }
+//     }
+//     return missing;
+//     }
+// let num = missing([0,7,4,5,6,4]);
+// console.log(num)
 
 
 ///output:- 2
+function count(arr1) {
+    let min = 1;
+    let max = 0;
+for(let i=0;i<arr1.length;i++) {
+    if(arr1[i] === arr1[i+1]) {
+        min++;
+    }
+    else if(min>max) {
+max = min;
+min = 0;
+    }
+
+   
+    // console.log(arr1[i])
+}
+
+max++;
+ return max;
+}
+let arr = count([0,0,1,1,1,1,1,1,2,4,1,1,1,1,1]);
+console.log(arr);
+
+////outout:-3
