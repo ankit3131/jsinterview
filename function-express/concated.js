a497fd3c390f98aeeeb19d7ace6843ab6e27351b
T8 <T8@CC.COM>
2026-07-27 11:41:02 +0530

    function express

--- function-express/concated.js
+++ function-express/concated.js
@@ -6,11 +6,13 @@ for(let i=0;i<arr.length;i++) {
         let left = arr.slice(0,j)
         let right = arr.slice(j)
 
-        console.log({left});
+        // console.log({left});
+        if(left.includes(add[i]) && right.includes(add[i])) {
+            debugger;
+    add.push(arr[i])
+}
     }
-//     if(arr.includes(arr[i])) {
-// add.push(arr[i])
-//     }
+
 }
 return add;
 }
