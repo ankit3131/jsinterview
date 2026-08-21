bc32cb4cde4ed446532748296425f49d1bc87b5d
Ankit Kumar <ankitchauhan21293@gmail.com>
2026-08-21 14:31:14 +0530

    map create

---
+++ map/create.js
@@ -0,0 +1,41 @@
+/////////////////////// step:-1
+
+// const users = [
+//     { firstName: "Ankit", lastName: "Kumar", age: 24 },
+//     { firstName: "Rahul", lastName: "Sharma", age: 30 }
+// ];
+// let array = users.map((user) => {
+// return {
+//     fullName : user.firstName +' ' + user.lastName ,
+//     age : user.age
+// }
+// }) 
+// console.log(array);
+
+//  output :- [
+//   { fullName: 'Ankit Kumar', age: 24 },
+//   { fullName: 'Rahul Sharma', age: 30 }
+// ]
+
+////////////////////////step:-2
+// const users = [
+//        { firstName: "Ankit", lastName: "Kumar", age: 24 },
+//     { firstName: "Rahul", lastName: "Sharma", age: 30 }
+// ];
+
+// let array = users.map((user) => {
+//     debugger;
+// let obj = {};
+// let string = "Fullname"
+// obj[string] = user.firstName + " " + user.lastName
+// obj.age =  user.age
+
+// return obj;
+// })
+
+// console.log(array);
+
+//  output :- [
+//   { fullName: 'Ankit Kumar', age: 24 },
+//   { fullName: 'Rahul Sharma', age: 30 }
+// ]
