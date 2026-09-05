a07ca1b4beeeb6ef54195682d34fa83cbff04b79
ankit3131 <ankitchauhan21293@gmail.com>
2026-09-05 21:26:00 +0530

    object and array

--- ObjectArray/totalamount.js
+++ ObjectArray/totalamount.js
@@ -1,3 +1,4 @@
+// step:1
 const orders = [
     {
         id:101,
@@ -10,12 +11,65 @@ const orders = [
     {
         id:102,
         customer:'Rahul',
-        item: [
+        items: [
             {name: 'Mobile',price:20000}
         ]
     }
 ]
+// let user = orders.map((value) => {
+// let valuese = value.items.reduce((accum , value) =>  {
+//     debugger;
+//     return accum + value.price
+//     },0)
+//     return {
+//         id:value.id,
+//         customer:value.customer,
+//         items:valuese
+//     }
+   
+// } 
 
+// )
+
+// output:-   {
+    //     id:101,
+    //     customer: 'Ankit',
+    //     items: 51000
+    // },
+
+      //     id:102,
+    //     customer: 'Ankit',
+    //     items: 20000
+    // },
+// console.log(user);
+
+// step:-2
+ function user(value) {
+ let store = [];
+    for(let i=0;i<value.length;i++) {
+         let obj = {};
+        debugger;
+         let price = 0;
+
+   for(let j=0;j<=value[i].items.length+1;j++) {
+    debugger;
+    const item = value[i].items[j]
+    if(item === undefined) { 
+        break;
+    }
+    price += value[i].items[j].price;
+   }
+   obj['id'] = value[i].id,
+   obj['customer'] = value[i].customer,
+   obj['items'] = price;
+           store.push(obj);
+      
+}
+  return store;
+ 
+}
+
+console.log(user(orders));
 // output:-   {
     //     id:101,
     //     customer: 'Ankit',
