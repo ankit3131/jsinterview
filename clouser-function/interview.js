8ca0826fa929ee6a7a487174a769f7edeea654e1
ankit3131 <ankitchauhan21293@gmail.com>
2026-09-20 21:14:59 +0530

    clouser function use

--- clouser-function/interview.js
+++ clouser-function/interview.js
@@ -23,20 +23,88 @@
 // 1
 // 2
 
-function bankAccount(balance) {
-    return {
-        deposit(amount) {
-            balance += amount;
-        },
+// function bankAccount(balance) {
+//     return {
+//         deposit(amount) {
+//             balance += amount;
+//         },
 
-        getBalance() {
-            return balance;
-        }
-    };
-}
+//         getBalance() {
+//             return balance;
+//         }
+//     };
+// }
 
-const account = bankAccount(1000);
+// const account = bankAccount(1000);
 
-account.deposit(500);
+// account.deposit(500);
 
-console.log(account.getBalance());
+// console.log(account.getBalance());
+
+// ouput:-1500
+
+// function outer() {
+//     debugger;
+//     let count = 0;
+//     return function() {
+//         debugger;
+//         count++;
+//         console.log(count)
+//     }
+// }
+// let a = outer();
+// let b = outer();
+// a();
+// a();
+// b();
+// a();
+// b();
+
+// output:-
+// 1
+// 2
+// 1
+// 3
+// 2
+
+// function outer() {
+//     let x = 10;
+
+//     return function() {
+//         x += 5;
+//         console.log(x)
+//     }
+
+// }
+// let fn = outer();
+// fn();
+// fn();
+// fn();
+
+// output:-
+
+// 15
+// 20
+// 25
+
+
+// function crateMultiplaer(number) {
+//     return function(value) {
+//         return number * value;
+//     }
+
+// }
+
+// let double = crateMultiplaer(5);
+// let triple = crateMultiplaer(3);
+// console.log(double(2));
+// console.log(triple(3));
+
+// output:-
+// 10 , 9
+
+let count = user(() => 
+    console.log("Hello"))
+count();
+count();
+count();
